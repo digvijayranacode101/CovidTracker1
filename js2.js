7ac86b88714a067aecc58f75c4ca8664e93a1fe1
digvijayranacode101 <50192999+digvijayranacode101@users.noreply.github.com>
2021-04-27 21:03:50 +0530

    Update js2.js

--- js2.js
+++ js2.js
@@ -73,20 +73,3 @@ function getCounts(){
 
 
 
-/*function renderHtml(data){
-    var htmlString = ""
-
-    for(let i =0; i<data.length; i++){
-        htmlString += "<p>" + data[i].name+" is a "+
-        data[i].species + " that  likes to eat " 
-        for(let ii =0; ii<data[i].foods.likes.length; ii++){
-            if(ii==0){
-                htmlString += data[i].foods.likes[ii]
-            }else{
-                htmlString += " and "+ data[i].foods.likes[ii]
-            }
-        }
-    }
-
-    animalContainer.insertAdjacentHTML('beforeend', htmlString)
-}*/
